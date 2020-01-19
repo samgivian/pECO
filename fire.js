a054b0b7d676d9bc6e3ee53a0a4b5d840cdef295
Saman Givian <samgivian2015@gmail.com>
2020-01-18 17:58:49 -0800

    Update fire.js

--- fire.js
+++ fire.js
@@ -1,14 +1,7 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgWzSXP7LHhUjEy2DiHH_2ypE0wVAFXp4",
-  authDomain: "peco-dfbe5.firebaseapp.com",
-  databaseURL: "https://peco-dfbe5.firebaseio.com",
-  projectId: "peco-dfbe5",
-  storageBucket: "peco-dfbe5.appspot.com",
-  messagingSenderId: "644045971636",
-  appId: "1:644045971636:web:f3604a44a641757ab4b7e1",
-  measurementId: "G-B8ZPHGTKH2"
+
 };
  let app= firebase.initializeApp(firebaseConfig);
   
